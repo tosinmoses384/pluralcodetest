135b42e7074181e85e6ff52800206313ca17e2e7
Israel <tosinmoses384@gmail.com>
2020-08-30 09:21:24 +0100

    first-commit

--- src/main.js
+++ src/main.js
@@ -2,12 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import SuiVue from 'semantic-ui-vue'
-import 'semantic-ui-css/semantic.min.css'
+
+import {
+  BootstrapVue,
+  BootstrapVueIcons
+  // SidebarPlugin
+} from 'bootstrap-vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCat, faDog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+library.add(faCat, faDog)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+// import SuiVue from 'semantic-ui-vue'
+// import 'semantic-ui-css/semantic.min.css'
 
 Vue.config.productionTip = false
 
-Vue.use(SuiVue)
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+// Vue.use(SidebarPlugin)
+
+// Vue.use(SuiVue)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,57 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+// %%%%%%%%%%%%%%%%%%%
+// NOT NEEDED FOR TEST
+// %%%%%%%%%%%%%%%%%%%%
 import home from '../views/home'
 import main from '../views/main'
 import initialhome from '../views/initialhome'
+
+// %%%%%%%%%%%%
+//  LOGIN PAGE
+// %%%%%%%%%%%
+import login from '../views/login'
+
 Vue.use(VueRouter)
+
+// const routes = [
+//   {
+//     path: '',
+//     name: 'home',
+//     component: home
+//   },
+//   {
+//     path: '/main',
+//     name: 'main',
+//     component: main,
+//     children: [
+//       { path: '/', component: initialhome },
+//       { path: '/initial/home', component: initialhome }
+//     ]
+//   }
+// ]
 
 const routes = [
   {
-    path: '',
+    path: '/',
+    name: 'login',
+    component: login
+  },
+  {
+    path: '/home',
     name: 'home',
     component: home
   },
   {
+    path: '/initialhome',
+    name: 'initialhome',
+    component: initialhome
+  },
+  {
     path: '/main',
     name: 'main',
-    component: main,
-    children: [
-      { path: '/', component: initialhome },
-      { path: '/initial/home', component: initialhome }
-    ]
+    component: main
   }
 ]
 
